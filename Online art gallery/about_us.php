cb3c2dadac91ca6a929a3550130821bf5457761c
Vattimalla-Arunkumar <arun102898@gmail.com>
2023-02-12 23:53:36 +0530

    First commit

---
+++ Online art gallery/about_us.php
@@ -0,0 +1,228 @@
+<?php
+include("includes/head.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<title>About Us</title>
+    <style type="text/css">
+           
+         body {
+                margin:0 auto;
+                height: 100%;
+                width: 100%;
+                background: url(pictures/sunny.jpg) no-repeat fixed;
+                background-size: cover;
+         }
+
+
+         .ART1 {
+         margin: 110px 0px 0px -60px;
+          font-size: 90px;
+          font-family: "Yu Gothic UI";
+          color: rgb( 45, 112, 213 );
+          text-align: center;
+          position: absolute;
+          top: -15px;
+          width: 215px;
+          height: 35px;
+          z-index: 19;
+        }
+
+        ._BAY1 {
+            margin: 110px 0px 0px 120px;
+          font-size: 90px;
+          font-family: "Yu Gothic UI";
+          color: rgb( 20, 20, 20);
+          text-align: center;
+          position: absolute;
+          top: -15px;
+          width: 215px;
+          height: 35px;
+          z-index: 19;
+        }
+
+        .Albay_1 {
+          margin: 250px 0px 0px 99px;
+          font-size: 30px;
+          font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+           z-index: 3;
+        }
+
+        .Original_1 {
+          margin: 250px 0px 0px 190px;
+          font-size: 30px;
+         font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+           z-index: 3;
+        }
+
+        .Artworks_1{
+
+          margin: 250px 0px 0px 325px;
+          font-size: 30px;
+          font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+          z-index: 3;
+        }
+          .border1{
+          border-radius: 8px;
+        float: left;
+        border: 1px solid none;
+        background-color: #112233;
+        padding: 150px 490px 0px 60px;
+        margin-left: 350px;
+        margin-top: 100px;
+
+    }
+     .border2{
+          border-radius: 8px;
+        float: left;
+        border: 1px solid none;
+        background-color: #112233;
+        padding: 420px 0px 0px 1160px;
+        margin-left: 50px;
+        margin-top: -20px;
+        margin-bottom: 0px;
+        position: absolute;
+        top:250px;
+        bottom: 0px;
+        opacity: 0.5;
+
+    }
+    .text1{
+            position: absolute;
+            color: rgb(255, 255, 255);
+            margin: 10px 5px 105px 5px;
+            text-align: justify;
+            font-family: "Yu Gothic UI Light";
+            font-weight: normal;
+            font-size: 20px;
+            top: 40px;
+            left: 50px;
+            right: 50px;
+            z-index: 3;
+
+        }
+         .head-the {
+          font-size: 30px;
+          font-family: "Yu Gothic UI Light";
+          color: rgb( 45, 112, 213 );
+          position: absolute;
+         margin: 700px 0px 100px 70px;
+
+          }
+        .head-staff{
+          font-size: 30px;
+         font-family: "Yu Gothic UI Light";
+          color: rgb( 00, 00, 00 );
+          position: absolute;
+           margin:700px 0px 100px 130px;
+          z-index: 19;
+        }
+        .hr{
+            position: absolute;
+            border: 1px solid #2d70d5;
+            top: 827px;
+            width: 1050px;
+            left: 73px;
+        }
+        .pics{
+          border-radius: 180px;
+          background-color: #1E5D9A;
+          float: left;
+          position: relative;
+          margin:750px 0px 100px -770px;
+          height: 230px;
+          width: 220px;
+        }
+        .pics2{
+          border-radius: 180px;
+          background-color: #1E5D9A;
+          float: left;
+          position: relative;
+          margin:750px 0px 100px -510px;
+          height: 230px;
+          width: 220px;
+        }
+         .pics3{
+          border-radius: 180px;
+          background-color: #1E5D9A;
+          float: left;
+          position: relative;
+          margin:750px 0px 100px -255px;
+          height: 230px;
+          width: 220px;
+        }
+          .pics4{
+          border-radius: 180px;
+          background-color: #1E5D9A;
+          float: left;
+          position: relative;
+          margin:750px 0px 100px -5px;
+          height: 230px;
+          width: 220px;
+        }
+          .text2{
+            position: absolute;
+            color: black;
+            text-align: justify;
+            font-family: "Yu Gothic UI Light";
+            font-weight: bold;
+            font-size: 18px;
+           top:1020px;
+           left: 152px;
+            z-index: 3;
+            margin-bottom: 100px;
+        }
+        .text3{
+            position: absolute;
+            color: #112233;
+            margin: 10px 5px 105px 5px;
+            text-align: justify;
+            font-family: "Yu Gothic UI Light";
+            font-weight: normal;
+            font-size: 15px;
+            top: 1050px;
+            left: 180px;
+            z-index: 3;
+        }
+
+    </style>
+
+<body>
+   
+<div id="content">
+        <h2 style="text-align: center; font-family: 'Courier New', Courier, monospace;">ABOUT US</h2>
+        <h1 style="text-align: center; color: lightgreen;">OUR VISION</h1>
+        <p style="text-align: center; font-size: larger; font-style: italic; font-family: Georgia, 'Times New Roman', Times, serif;">The ART.gallery is a dynamic and creative hub and the focal point for artists, collectors, and institutional figures, where art meets design and academic style borders on the revolutionary present. Our Gallery is a global online platform for Art & Design, an international marketing channel creating leads for our artists, while at the same time offering a branding solution to them. Think of us as a fast-growing international atelier.    
+        </p>
+        <p style="text-align: center; font-size: larger; font-style: italic; font-family: Georgia, 'Times New Roman', Times, serif;">Art defines the creation of something with imagination and skill expressing important ideas or feelings; in our Gallery, you will find a wide variety of artistic objects made by independent artisan creators around the world, from the eccentric groundbreaking contemporary artworks to the delicacy of classical fine art paintings.
+        </p>
+        <p style="text-align: center; font-size: larger; font-style: italic; font-family: Georgia, 'Times New Roman', Times, serif;">Art has no limits. Anything from a line on a piece of paper to a unique handcrafted wooden object or a photo capture can be called an art-piece. Discover cutting-edge designs and emerging contemporary artists.
+        </p>
+        <h1 style="text-align: center; color: lightskyblue;">OUR MISSION</h1>
+        <p style="text-align: center; font-size: larger; font-style: italic; font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;">As an Art Institution, our aim is to promote up-coming revolutionary artists, along with presenting artists already established in the international art community. We provide further opportunities for artists to introduce their collections, exhibitions, installations, and other unique artistic projects in an intimate online environment globally, through our website, our social media pages, and via monthly (occasionally bi-weekly) newsletters.
+        </p>
+        <p style="text-align: center; font-size: larger; font-style: italic; font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;">Our mission is to form a solid ground for the future development and distribution of artworks and ideas, to form a new online art community.
+        </p>
+
+        </div>
+  
+</body>
+</html>
